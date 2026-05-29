b32100cc19a499fc751d19ee4eb24e56f8edb9e1
User Dev <user@example.com>
2026-05-29 14:57:01 +0200

    Support commmand line arguments.

---
+++ autorun.bat
@@ -0,0 +1,16 @@
+@REM To run autorun.bat in powershell, run the following command:
+@REM Start-Process -FilePath "autorun.bat" -WorkingDirectory "C:\Users\l\Documents\PTCGPB_Leanny" -NoNewWindow
+
+@REM Define variables
+set AutoHotkeyPath="C:\Program Files\AutoHotkey\AutoHotkey.exe"
+set PTCGPBPath="C:\Users\l\Documents\PTCGPB_Leanny"
+
+@REM Start all mumu
+%AutoHotkeyPath% "%PTCGPBPath%\Scripts\Include\LaunchAllMumu.ahk"
+
+@REM Wait for all mumu to start
+timeout /t 60
+
+@REM Execute a specific script without using the UI. This will run BalanceXML first. Supported commands :
+@REM CreateBots13P, Inject13P, InjectWonderpick96P, InjectRewards
+%AutoHotkeyPath% "%PTCGPBPath%\PTCGPB.ahk" clicommand Inject13P
